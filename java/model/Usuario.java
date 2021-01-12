04b3c20ce19081c5448a0eced7307b7dab1a8e56
Fernando Goncalves <fgoncalves624@gmail.com>
2021-01-12 16:17:27 -0300

    Inicio do projeto: formulario de cadastro, css simples, usuario-controller.js com assinatura de funcoes e modelo de usuario

---
+++ java/model/Usuario.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Usuario {
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private String senha;
+	private String sexo;
+	private String endereco;
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/webapp/js/usuario-controller.js
@@ -0,0 +1,14 @@
+usuarioController = function(){
+
+
+    this.aoClicarSalvar = function(){
+        window.alert("Clicou em salvar");
+    }
+    this.aoClicarCancelar = function(){
+        window.alert("Clicou em cancelar");
+    }
+    this.aoClicarEditar = function(){}
+    this.aoClicarExcluir = function(){}
+
+
+}
